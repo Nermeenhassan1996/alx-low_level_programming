a0cb71dd01260d0bbf3bf3c4b9c9cf336c2e958e
Nermeen Hassan <nermeenhassan1996@gmail.com>
2023-08-12 18:14:18 -0700

    script 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - prints the digits from 0 to 9.
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
