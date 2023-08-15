3d8b72058bc19b412a37eb3f9939e648def8eba5
Nermeen Hassan <nermeenhassan1996@gmail.com>
2023-08-15 15:30:48 -0700

    script 3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,12 +1,11 @@
 #include "main.h"
 
-/*
+/**
  * _islower - Check description
  * @c: An input character
  * Description: checks if the c is lower case or not
  * Return: 1 if true 0 if false
  */
-
 int _islower(int c)
 {
 	int isLower = 0;
