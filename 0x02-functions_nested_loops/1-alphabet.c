1e00113c93dfb7229a5378bf069b05b09a5441ba
Nermeen Hassan <nermeenhassan1996@gmail.com>
2023-08-15 14:18:27 -0700

    script 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,9 @@
 #include "main.h"
 
-/*
- * function for printing the alphabet in lower case
+/**
+ * print_alphabet - Check description
+ * Description: It prints the alphabet in lowercase fallowed by a new line
+ * Return: void
  */
 
 void print_alphabet(void)
@@ -9,7 +11,7 @@ void print_alphabet(void)
 	char alphabet[] = "abcdefghijklmnopqrstuvwxyz";
 	int i = 0;
 
-	for(; i <= 25; i++)
+	for (; i <= 25; i++)
 	{
 		_putchar(alphabet[i]);
 	}
