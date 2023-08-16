ac722d377d8b1b5e35ee6c04b9d42308eb482fc8
Nermeen Hassan <nermeenhassan1996@gmail.com>
2023-08-15 18:26:04 -0700

    script 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - check description
+ * @x: an integer input
+ * @y: an integer input
+ * Description: takes two integers and add them
+ * and returns result
+ * Return: returns integer value
+ */
+int add(int x, int y)
+{
+	return (x + y);
+}
