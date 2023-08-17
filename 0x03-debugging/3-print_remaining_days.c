da6aeb23bb6333b5c0c06d1cc4761aba6c5cf32d
Nermeen Hassan <nermeenhassan1996@gmail.com>
2023-08-17 15:05:56 -0700

    script 3

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -16,20 +16,29 @@ void print_remaining_days(int month, int day, int year)
 
 	if (year % 4 == 0)
 	{
-		dayConverted = convert_day(month, day) + 1;
+		if (month == 2 && day > 29)
+		{
+			printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
+		}
+		else
+		{
+			dayConverted = convert_day(month, day);
+			if (month == 2 && day < 29)
+				dayConverted += 1;
 
-		printf("Day of the year: %d\n", dayConverted);
-		printf("Remaining days: %d\n", 366 - dayConverted);
+			printf("Day of the year: %d\n", dayConverted);
+			printf("Remaining days: %d\n", 366 - dayConverted);
+		}
 	}
 	else
 	{
-		dayConverted = convert_day(month, day);
 		if (month == 2 && day >= 29)
 		{
 			printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
 		}
 		else
 		{
+			dayConverted = convert_day(month, day);
 			printf("Day of the year: %d\n", dayConverted);
 			printf("Remaining days: %d\n", 365 - dayConverted);
 		}
