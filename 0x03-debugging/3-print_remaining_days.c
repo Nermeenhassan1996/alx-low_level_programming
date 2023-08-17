f4edacafa0cd6ef618d346b7c605139bcb68a512
Nermeen Hassan <nermeenhassan1996@gmail.com>
2023-08-17 15:49:29 -0700

    script 3

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -12,26 +12,28 @@
 
 void print_remaining_days(int month, int day, int year)
 {
-	if (year % 4 == 0 || (year % 400 == 0 && !(year % 100 == 0)))
-	{
-		if (month >= 2)
-		{
-			day++;
-		}
+	int dayConverted;
 
-		printf("Day of the year: %d\n", day);
-		printf("Remaining days: %d\n", 366 - day);
+	if (year % 4 == 0)
+	{
+		dayConverted = convert_day(month, day);
+		if ((month == 2 && day < 29) || month > 2)
+			dayConverted += 1;
+
+		Console.WriteLine("Day of the year: {0}\n", dayConverted);
+		Console.WriteLine("Remaining days: {0}\n", (366 - dayConverted));
 	}
 	else
 	{
-		if (month == 2 && day == 60)
+		if (month == 2 && day >= 29)
 		{
-			printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
+			Console.WriteLine("Invalid date: {0}/{1}/{2}\n", month, day, year);
 		}
 		else
 		{
-			printf("Day of the year: %d\n", day);
-			printf("Remaining days: %d\n", 365 - day);
+			dayConverted = convert_day(month, day);
+			Console.WriteLine("Day of the year: {0}\n", dayConverted);
+			Console.WriteLine("Remaining days: {0}\n", (365 - dayConverted));
 		}
 	}
 }
