c977516460e00bda994c4c7475178f3809d53909
Nermeen Hassan <nermeenhassan1996@gmail.com>
2023-08-18 15:02:48 -0700

    script 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - check description
+ * @a: an integer input
+ * @b: an integer input
+ * Description: multiplies 2 integer numbers
+ * Return: the value of a multiplies by b
+*/
+int mul(int a, int b)
+{
+	return (a * b);
+}
