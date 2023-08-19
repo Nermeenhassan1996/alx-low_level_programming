ac20524a2585cf0a062aebfdc6e9be903d0200b5
Nermeen Hassan <nermeenhassan1996@gmail.com>
2023-08-18 18:40:22 -0700

    script 101

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,36 +1,36 @@
 #include "main.h"
+void print_int(int n);
 
 /**
- * print_number - check description
+ * print_number - function to prints an integer.
  * @n: an integer input
- * Description: prints the given integer
  * Return: nothing
-*/
+ */
 void print_number(int n)
 {
-	int reversed = 0;
-
 	if (n == 0)
-	{
 		_putchar('0');
-		return;
-	}
-
-	if (n < 0)
+	else if (n < 0)
 	{
 		_putchar('-');
-		n *= -1;
+		print_int(n * -1);
 	}
+	else
+		print_int(n);
+}
 
-	while (n > 0)
-	{
-		reversed = reversed * 10 + n % 10;
-		n /= 10;
-	}
+/**
+ * print_int - function to print n
+ * @n: an integer input
+ * Return: nothing
+ */
+void print_int(int n)
+{
+	int i = 1000000000;
 
-	while (reversed > 0)
+	for (; i >= 1; i /= 10)
 	{
-		_putchar('0' + (reversed % 10));
-		reversed /= 10;
+		if (n / i != 0)
+			_putchar((n / i) % 10 + '0');
 	}
 }
