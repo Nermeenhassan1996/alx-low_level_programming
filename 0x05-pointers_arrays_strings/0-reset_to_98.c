5b9aa3c2e2c5cd0266237fb1b84a57b1df22ca75
Nermeen Hassan <nermeenhassan1996@gmail.com>
2023-08-22 12:04:13 -0700

    script 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - check description
+ * Description: function that updates an integer pointer value to 98
+ * @n: an integer input pointer
+ * Return: nothing.
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
