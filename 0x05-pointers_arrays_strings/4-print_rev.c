ff934b7eaf58adff506f9dd93f90d4afe316ab15
Nermeen Hassan <nermeenhassan1996@gmail.com>
2023-08-22 12:58:49 -0700

    script 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_rev - chech description
+ * Description: function that prints a string, in reverse,
+ * followed by a new line.
+ * @s: an input string
+ * Return: Nothing
+ */
+void print_rev(char *s)
+{
+	int len = 0;
+
+	while (s[len] != '\0')
+		len++;
+
+	for (; len >= 0; len--)
+		_putchar(s[len]);
+
+	_putchar('\n');
+}
