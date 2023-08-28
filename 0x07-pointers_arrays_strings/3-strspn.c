f734dd5dbf5a0a2e16d5b1f9aed1e621737d8f8a
Nermeen Hassan <nermeenhassan1996@gmail.com>
2023-08-28 14:48:41 -0700

    script 2

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * _strspn - check description
+ * Description: a function that gets the length of a prefix substring.
+ * @s: an input string
+ * @accept: an input character with to locate into string s
+ * Return: returns pointer to c position
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int count = 0;
+       	int flag = 0;
+	char *start = accept;
+
+	while (*s)
+	{
+		flag = 0;
+		while (*accept)
+		{
+			if (*accept == *s)
+			{
+				count++;
+				flag = 1;
+				break;
+			}
+			accept++;
+		}
+		s++;
+		accept = start;
+		if (flag == 0)
+			break;
+	}
+	return (count);
+}
